ad5f687876772c2e6b9bf97f2d0e9877fd68c7a0
Mathieu Legrand <mathieu.legrand78@gmail.com>
2025-08-05 09:01:32 +0200

    suppression d'espace

--- frontend/components/Home.js
+++ frontend/components/Home.js
@@ -7,6 +7,7 @@ import dynamic from "next/dynamic";
 import { useState } from "react";
 
 // Ce composant sera uniquement rendu côté client, permet de bouger sur la map et ne pas laisser la carte en static
+// et permet d'enelver l'erreur ReferenceError: window is not defined car Phaser utilise utilise des objets liés au DOM qui n'existent aps côté server
 const PhaserGame = dynamic(() => import("./PhaserGame"), {
   ssr: false, // 
 });
