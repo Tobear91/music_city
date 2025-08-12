84529b5ee14ff457ac62ddd7bbb4f746eaa7c9e5
Camille HUrtaud <camille.hurtaud@gmail.com>
2025-08-12 16:05:33 +0200

    Fix CSS sur la Card Vinyle

--- frontend/components/VinylesStore/Card.js
+++ frontend/components/VinylesStore/Card.js
@@ -8,15 +8,17 @@ function Card({ index, item, deleteRelease }) {
       <div>
         <img src={item.thumb} alt={item.title} />
       </div>
-      <h2>{item.title}</h2>
-      <p>{item.artist}</p>
       <div>
-        <a className="button-square small purple" href={`http://127.0.0.1:3001/vinyles-store/release/${item.release_id}`}>
-          <FontAwesomeIcon icon={faEye} />
-        </a>
-        <button className="button-square small red" onClick={() => deleteRelease(item.release_id, index)}>
-          <FontAwesomeIcon icon={faTrash} />
-        </button>
+        <h2>{item.title}</h2>
+        <p>{item.artist}</p>
+        <div>
+          <a className="button-square small purple" href={`http://127.0.0.1:3001/vinyles-store/release/${item.release_id}`}>
+            <FontAwesomeIcon icon={faEye} />
+          </a>
+          <button className="button-square small red" onClick={() => deleteRelease(item.release_id, index)}>
+            <FontAwesomeIcon icon={faTrash} />
+          </button>
+        </div>
       </div>
     </div>
   );
